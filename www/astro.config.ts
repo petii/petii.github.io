b3abd498040a42b7513be5d50f9e5ecf45976ee7
petii <mail@petii.dev>
2025-11-29 12:13:28 +0100

    play around with an ats optimized cv version

--- www/astro.config.ts
+++ www/astro.config.ts
@@ -2,6 +2,7 @@
 
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
@@ -12,4 +13,5 @@ export default defineConfig({
     "/": "/coming-soon",
     "/persona": "/persona/programmer",
   },
+  integrations: [mdx()]
 });
--- www/src/data/personas/programmer.md
+++ www/src/data/personas/programmer.md
@@ -5,3 +5,6 @@ profilePicUrl: '/assets/img/2020-01-20.jpeg'
 ---
 
 something something
+
+check out my [cv if you're a human](/resume) 
+download my [cv "designed" for ats](/resume/ats)
